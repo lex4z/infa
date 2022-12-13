aec87f37ba04d19102efc92f1f13ad951c98aa99
lex4z <78318213+lex4z@users.noreply.github.com>
2022-12-13 11:06:57 +0300

    hj

--- kkkk.cpp
+++ kkkk.cpp
@@ -48,15 +48,16 @@ void fano(el* a, int s, int f){
 	int d = 0, l = 0;
 	float k = 0;
 	for(int i = s; i < f; i++) d += a[i].n;
-	if((float) a[s].n/d > 0.5 && f-s==2){
+	if((float) a[s].n/d > 0.5){
 		addBack(a[s].code, '0');
-		addBack(a[s+1].code, '1');
-		return;
-	}
-	for(int i = s; i < f; i++){
-		k += (float) a[i].n/d;
-		addBack(a[i].code, 48 + (k > 0.5f + 1./(d+a[i].n)));
-		l += 1 - (k > 0.5f + 1./(d+a[i].n));
+		for(int i = s+1; i < f; i++) addBack(a[i].code, '1');
+		l=1;
+	}else {
+		for(int i = s; i < f; i++){
+			k += (float) a[i].n/d;
+			addBack(a[i].code, 48 + (k > 0.5f + 1./(d*2)));
+			l += 1 - (k > 0.5f + 1./(d*2));
+		}
 	}
 	
 	fano(a, s,s+l);
@@ -116,8 +117,17 @@ char* decompression(char* str, el* m, int k){
 	n = 0;
 	while(t[n] != '\0'){
 		addBack(code, t[n]);
+		int f = 0;
+		char sym;
 		for(int j = 0; j < k; j++) {
-			
+			if(areEqual(code, m[j].code)){
+				sym = m[j].c;
+				f++;
+			}
+		}
+		if(f==1){
+			addBack(ans, sym);
+			code[0]='\0';
 		}
 		n++;
 	}
@@ -126,7 +136,7 @@ char* decompression(char* str, el* m, int k){
 }
 
 int main() {
-	char str[200] = "aa bbb ccddddd";
+	char str[200] = "asdfghjk";
 	int k = 0, m[256]={0}, len = 0;
 	while(str[len] != '\0'){
 		m[str[len]]++;
@@ -158,7 +168,6 @@ int main() {
 		if(f) break;
 	}
 	
-	for(int i = 0; i < k; i++)printf("%c - %d\n",ans[i].c, ans[i].n);
 	fano(ans,0,k);
 	for(int i = 0; i < k; i++)printf("%c - %s\n",ans[i].c, ans[i].code);
 	
@@ -166,7 +175,10 @@ int main() {
 	puts(encodedStr(ans, str, k));
 	char* ii = compressedStr(encodedStr(ans, str, k));
 	int a = 0;
-	
+	while(ii[a]!='\0') {
+		printf("%s\n", bin(ii[a]));
+		a++;
+	}
 	puts(decompression(compressedStr(encodedStr(ans, str, k)),ans,k));
 	
 	return 0;
