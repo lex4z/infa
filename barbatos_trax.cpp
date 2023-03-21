a1d4673dc81bec12e5b390a53fec4623c33f52cb
lex4z <78318213+lex4z@users.noreply.github.com>
2023-03-21 22:21:21 +0300

    Update barbatos_trax.cpp

--- barbatos_trax.cpp
+++ barbatos_trax.cpp
@@ -9,28 +9,49 @@ struct el{
 
 int bart[255];
 
+void addBack(char* str, char c){
+	int n = 0;
+	while(str[n]!='\0') n++;
+	str[n]=c;
+	str[n+1]='\0';
+}
+
+char* bin(unsigned char c){
+	char* s = new char[9];
+	for(int i = 0; i < 8; i++) s[i] = 48 + (c>>(8-i)&1);
+	s[8] = '\0';
+	return s;
+}
+
 int main() {
 	FILE* f = fopen("mama.txt", "r");
-	int j, max = 0, k = 0;
-	while((j = getc(f)) != EOF) bart[j]++;
+	int ch, k = 0;
+	while((ch = getc(f)) != EOF) bart[ch]++;
+	el* m = new el[k];
 	for(int i = 0; i < 255; i++){
 		if(bart[i]) {
-			printf("%c: %d\n",i,bart[i]);
-			k++;
+			//printf("%c: %d\n",i,bart[i]);
+			el* t = new el[k++];
+			for(int j = 0; j < k-1; j++) t[j]=m[j];
+			t[k-1].c = i;
+			t[k-1].n = bart[i];
+			delete [] m;
+			m = t;
 		}
 	}
 	
-	el* m = new el[k];
-	k = 0;
+	//el* m = new el[k];
+	//k = 0;
+	/*
 	for(int i = 0; i < 255; i++){
 		if(bart[i]){
 			m[k].c = i;
 			m[k].n = bart[i];
 		}
 	}
-	
+	*/
 	for(int i = 0; i < k -1; i++){
-		for(int j = 0; j < k - i - 1){
+		for(int j = 0; j < k - i - 1; j++){
 			if(m[j].n < m[j+1].n){
 				el temp = m[j];
 				m[j] = m[j+1];
@@ -38,6 +59,9 @@ int main() {
 			}
 		}
 	}
+	
+	for(int i = 0; i < k; i++) printf("%c: %d\n",m[i].c, m[i].n);
+	
 	
 	printf("%d\n",k);
 	return 0;
